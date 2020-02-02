bba10b898b2921db93be7f3fb7e18b014f746aaf
Andrew Potter <apottere@gmail.com>
2020-02-02 04:12:58 -0500

    Updates

---
+++ precache-manifest.61fd9024a869ca9a891da1043c7b0384.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb733b7ff7f973b3d376a8b135ba1a4",
+    "url": "/pandemic-helper/index.html"
+  },
+  {
+    "revision": "1d2cc19d36f299a27d50",
+    "url": "/pandemic-helper/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "69bed3c0f76c2502beed",
+    "url": "/pandemic-helper/static/js/2.5d487aa8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pandemic-helper/static/js/2.5d487aa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d2cc19d36f299a27d50",
+    "url": "/pandemic-helper/static/js/main.0df640bb.chunk.js"
+  },
+  {
+    "revision": "4add3a89bbef605e04d4",
+    "url": "/pandemic-helper/static/js/runtime-main.9a139f6d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pandemic-helper/static/media/logo.5d5d9eef.svg"
+  }
+]);
