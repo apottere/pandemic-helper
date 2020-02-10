419ca9470d2d6a383af7e6fd8501c11e186c8665
Andrew Potter <apottere@gmail.com>
2020-02-10 11:47:03 -0500

    Remove key

--- src/infection/footer.js
+++ src/infection/footer.js
@@ -1,6 +1,6 @@
 import { createUseStyles } from 'react-jss';
 import { useAppState } from '../state';
-import { AlertTriangle, CapslockFill, SkipBackward, SkipForward, Trash } from 'react-bootstrap-icons';
+import { SkipBackward, SkipForward, Trash } from 'react-bootstrap-icons';
 import { Button } from 'react-bootstrap';
 import React from 'react';
 
@@ -17,28 +17,12 @@ const useStyles = createUseStyles({
         bottom: 0,
         width: '100%',
     },
-    key: {
-        textAlign: 'center',
-        flexGrow: 1,
-        padding: '0 15px',
-        borderRight: '1px solid #5f5f5f',
-
-        '& > span': {
-            '&:first-child': {
-                paddingRight: '15px'
-            },
-            '&:last-child': {
-                paddingLeft: '15px'
-            },
-            '& > svg': {
-                marginBottom: '1px'
-            }
-        }
-    },
     buttons: {
         padding: '0 15px',
+        flexGrow: 1,
+        textAlign: 'center',
         '& button': {
-            marginRight: '5px',
+            marginRight: '15px',
             '&:last-child': {
                 marginRight: 0
             }
@@ -52,11 +36,6 @@ export const Footer = () => {
 
     return (
         <div className={`d-flex ${styles.footer}`}>
-            <span className={`mr-auto text-muted ${styles.key}`}>
-                <span><CapslockFill size={20} /> Infect</span>
-                |
-                <span><AlertTriangle size={20} /> Epidemic</span>
-            </span>
             <span className={styles.buttons}>
                 <Button variant="outline-primary" onClick={() => window.history.back()}><SkipBackward size={24} /></Button>
                 <Button variant="outline-danger" onClick={() => setState({})}><Trash size={24} /></Button>
